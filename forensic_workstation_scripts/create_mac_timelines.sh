b9cffb7ffd04a3c22a1cbca6f9edea1403c4345a
Kanishk Sharan <32040557+kanishksharan12@users.noreply.github.com>
2018-03-08 13:25:48 -0800

    Create create_mac_timelines.sh

---
+++ forensic_workstation_scripts/create_mac_timelines.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Author: Kanishk Sharan
+
+"""
+Simple shell script to extract MAC times from an image to
+a CSV file. This can further be import into a spreadsheet.
+"""
+
+usage_type () {
+	echo "usage: $0 <starting directory>"
+	echo "Simple script to get MAC times from an image and output CSV"
+	exit 1
+}
+
+if [ $# -lt 1 ] ; then
+   usage_type
+fi
+
+"""
+semicolon delimited file which makes import to spreadsheet easier printf is access date, access time, modify date, modify time,
+create date, create time, permissions, user id, user name, group id, group name, file size, filename and then line feed
+"""
+
+olddir=$(pwd)
+cd $1 # this avoids having the mount point added to every filename
+printf "Access Date;Access Time;Modify Date;Modify Time;Create Date;Create Time;Permissions;User ID;Group ID;File Size;Filename\n"
+find ./ -printf "%Ax;%AT;%Tx;%TT;%Cx;%CT;%m;%U;%G;%s;%p\n"
+cd $olddir
